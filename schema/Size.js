ab64d15a0908f84cdfcc5cb0ee1a07ac40729950
FLYBYME <mangoraft@gmail.com>
2018-05-21 08:36:28 -0400

    updated size for new port

--- schema/Size.js
+++ schema/Size.js
@@ -2,62 +2,60 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SizeSchema = new Schema({
-	type : {
-		type : String,
-		required : true
-	},
-	system : {
-		type : Boolean,
-		required : true,
-		'default' : false
-	},
-	created_at : {
-		type : Date,
-		required : true,
-		'default' : Date.now
-	},
-	updated_at : {
-		type : Date,
-		required : true,
-		'default' : Date.now
-	},
-	memory : {
-		type : Number,
-		required : true
-	},
-	cpuset : {
-		type : Number,
-		required : true
-	},
-	cpuShares : {
-		type : Number,
-		required : true
-	},
-	memoryReservation : {
-		type : Number,
-		required : true
-	},
-	ioMaximumBandwidth : {
-		type : Number,
-		required : true
-	},
-	ioMaximumIOps : {
-		type : Number,
-		required : true
-	},
-	oomKillDisable : {
-		type : Boolean,
-		required : true
-	},
-	dedicated : {
-		type : Boolean,
-		required : true,
-		'default' : false
-	}
+    type: {
+        type: String,
+        required: true
+    },
+    system: {
+        type: Boolean,
+        required: true,
+        'default': false
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        'default': Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        'default': Date.now
+    },
+    memory: {
+        type: Number,
+        required: true
+    },
+    cpu: {
+        type: Number,
+        required: true
+    },
+    memoryReservation: {
+        type: Number,
+        required: true
+    },
+    io: {
+        bandwidth: {
+            type: Number,
+            required: true
+        },
+        iops: {
+            type: Number,
+            required: true
+        }
+    },
+    oomKillDisable: {
+        type: Boolean,
+        required: true
+    },
+    dedicated: {
+        type: Boolean,
+        required: true,
+        'default': false
+    }
 });
 
-SizeSchema.pre('save', function(next) {
-	this.updated_at = new Date();
-	next();
+SizeSchema.pre('save', function (next) {
+    this.updated_at = new Date();
+    next();
 });
 module.exports = mongoose.model('Size', SizeSchema);
